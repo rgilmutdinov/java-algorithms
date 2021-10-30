9ac18549c6b6b2cc07823b87de9b523f15101a20
Ruslan Gilmutdinov <gilmutdinov.ruslan@gmail.com>
2021-10-30 19:27:52 +0600

    add binary search

---
+++ binary-search/BinarySearch.java
@@ -0,0 +1,72 @@
+class Solution {
+    public int findLeftmost(int[] nums, int target, int lo, int hi) {
+        if (lo > hi) return -1;
+
+        hi++;
+        int end = hi;                
+        while (lo < hi) {            
+            int mid = lo + (hi - lo) / 2;            
+            if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        if (lo != end && nums[lo] == target) {
+            return lo;
+        }
+
+        return -1;
+    }
+    
+    public int findRightmost(int[] nums, int target, int lo, int hi) {
+        if (lo > hi) return -1;
+        
+        hi++;
+        int end = hi; 
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (target < nums[mid]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        if (lo != end && nums[lo] == target) {
+            return lo;
+        }
+        
+        return -1;
+    }
+	
+	public int bisectLeft(int[] nums, int target, int lo, int hi) {
+        hi++;
+        while (lo < hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return lo;
+    }
+
+    public int bisectRight(int[] nums, int target, int lo, int hi) {
+        hi++;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (target < nums[mid]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+}
